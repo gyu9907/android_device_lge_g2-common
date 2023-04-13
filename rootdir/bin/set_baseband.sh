85ea515f5c5852152ccb7c1bd2d299e15a0ac1cd
gyu9907 <gyu9907@gmail.com>
2023-04-14 02:30:48 +0900

    vu3: add f300 support

--- rootdir/bin/set_baseband.sh
+++ rootdir/bin/set_baseband.sh
@@ -2,7 +2,7 @@
 
 # grep the modem partition for baseband version and set it
 if strings /firmware/image/modem.b21 | grep "^M8974A-" >> /dev/null; then
-    # "d800" | "d801" | "d802" | "d803" | "f320" | "l01f"
+    # "d800" | "d801" | "d802" | "d803" | "f300" |  "f320" | "l01f"
     setprop gsm.version.baseband `strings /firmware/image/modem.b21 | grep "^M8974A-" | head -1`
 elif strings /firmware/image/modem.b18 | grep "^MPSS.DI" >> /dev/null; then
     # "ls980" | "vs980"
