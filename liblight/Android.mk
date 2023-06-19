a7f5f1955668b091ba1a2c771eb186fc0b117267
gyu9907 <gyu9907@gmail.com>
2023-06-20 01:58:40 +0900

    f300: Fix conflict with module for f300
    Change-Id: I3b70e585006b6b6695cab954c0aa997a86363fd0

--- liblight/Android.mk
+++ liblight/Android.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+ifneq ($(filter g2 d800 d801 d802 d803 f320 ls980 vs980,$(TARGET_DEVICE)),)
+
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -22,3 +24,5 @@ LOCAL_MODULE := lights.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+endif
